985bcc9b4d84edc1555e7963057de4ee7329b58f
GustavsLieknins <126756140+GustavsLieknins@users.noreply.github.com>
2024-11-14 12:58:54 +0200

    123

--- warehouse/app/Http/Controllers/Auth/PasswordResetLinkController.php
+++ warehouse/app/Http/Controllers/Auth/PasswordResetLinkController.php
@@ -26,7 +26,7 @@ class PasswordResetLinkController extends Controller
     public function store(Request $request): RedirectResponse
     {
         $request->validate([
-            'email' => ['required', 'email'],
+            'username' => ['required'],
         ]);
 
         // We will send the password reset link to this user. Once we have attempted
@@ -39,6 +39,6 @@ class PasswordResetLinkController extends Controller
         return $status == Password::RESET_LINK_SENT
                     ? back()->with('status', __($status))
                     : back()->withInput($request->only('email'))
-                        ->withErrors(['email' => __($status)]);
+                        ->withErrors(['username' => __($status)]);
     }
 }
